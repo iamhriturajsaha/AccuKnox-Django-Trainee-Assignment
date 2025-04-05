d5bdb3d5b8148b631b3dfa7a7b2117ae35e42af4
Hrituraj Saha <156566581+iamhriturajsaha@users.noreply.github.com>
2025-04-05 13:11:22 +0530

    Add files via upload

---
+++ Q3 Views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.db import transaction
+from .models import Book
+import logging
+
+logger = logging.getLogger(__name__)
+
+def create_book_view(request):
+    try:
+        with transaction.atomic():
+            Book.objects.create(title="Transactional Signal Test")
+            raise Exception("💥 Simulated exception to trigger rollback")
+    except Exception as e:
+        logger.warning(f"❌ Exception occurred: {e}")
+    return HttpResponse("Done")
